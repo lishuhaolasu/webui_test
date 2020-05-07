4ee7c12ba8c8d5a02cef13ea230126914aefdc8d
lishuhaolasu <32184932+lishuhaolasu@users.noreply.github.com>
2020-05-07 20:07:49 +0800

    增加linux支持

--- conftest.py
+++ conftest.py
@@ -4,7 +4,7 @@
 """
 import os
 import time
-
+import sys
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -14,15 +14,19 @@ from common_utils.pathconf import BINS_DIR
 
 @pytest.fixture(scope='class')
 def get_browser_cls():
-        driver_path = f'chromedriver_{conf.get("common","driver_version")}.exe'
         opt = Options()
+        if sys.platform == 'win32':
+            driver_path = f'chromedriver_{conf.get("common","driver_version")}.exe'
+        elif sys.platform == 'linux':
+            driver_path = f'chromedriver_{conf.get("common","driver_version")}'
+            opt.binary_location('/usr/bin/chromium-browser')
+            opt.add_argument('--headless')
         opt.add_argument('--no-sandbox')
         opt.add_argument('window-size=1920x1080')
         opt.add_argument('--disable-gpu')
         opt.add_argument('--hide-scrollbars')
         opt.add_argument('blink-settings=imagesEnabled=false')
         opt.add_argument('user-agent="MQQBrowser/26 Mozilla/5.0 (Linux; U; Android 2.3.7; zh-cn; MB200 Build/GRJ22; CyanogenMod-7) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1"')
-        # opt.add_argument('--headless')
         driver = webdriver.Chrome(executable_path=os.path.join(BINS_DIR, driver_path),chrome_options=opt)
         wait_timeout = int(conf.get('common', 'driver_timeout'))
         driver.implicitly_wait(wait_timeout)
@@ -31,15 +35,19 @@ def get_browser_cls():
         driver.quit()
 @pytest.fixture(scope='function')
 def get_browser_func():
-        driver_path = f'chromedriver_{conf.get("common","driver_version")}.exe'
         opt = Options()
+        if sys.platform == 'win32':
+            driver_path = f'chromedriver_{conf.get("common","driver_version")}.exe'
+        elif sys.platform == 'linux':
+            driver_path = f'chromedriver_{conf.get("common","driver_version")}'
+            opt.binary_location('/usr/bin/chromium-browser')
+            opt.add_argument('--headless')
         opt.add_argument('--no-sandbox')
         opt.add_argument('window-size=1920x1080')
         opt.add_argument('--disable-gpu')
         opt.add_argument('--hide-scrollbars')
         opt.add_argument('blink-settings=imagesEnabled=false')
         opt.add_argument('user-agent="MQQBrowser/26 Mozilla/5.0 (Linux; U; Android 2.3.7; zh-cn; MB200 Build/GRJ22; CyanogenMod-7) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1"')
-        # opt.add_argument('--headless')
         driver = webdriver.Chrome(executable_path=os.path.join(BINS_DIR, driver_path),chrome_options=opt)
         wait_timeout = int(conf.get('common', 'driver_timeout'))
         driver.implicitly_wait(wait_timeout)
