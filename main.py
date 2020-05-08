03da9a30e7beacf752233d1fa94e205b517f316f
lishuhaolasu <lishuhao@51ias.com>
2020-05-08 12:12:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ from common_utils.path_lib import CASE_DIR, REPORT_DIR
 from common_utils import getdatepath
 
 if __name__ == '__main__':
-    date_now = time.strftime('%Y%m%d-%H%M%S',time.localtime(time.time()))
-    full_report_filename = getdatepath(prefix=REPORT_DIR,filename=f'report-{date_now}.html')
-    pytest.main(['--html',full_report_filename,'-m','smoke','--reruns','3','--alluredir',REPORT_DIR])
+    # date_now = time.strftime('%Y%m%d-%H%M%S',time.localtime(time.time()))
+    # full_report_filename = getdatepath(prefix=REPORT_DIR,filename=f'report-{date_now}.html')
+    pytest.main(['-m','smoke','--reruns','3','--alluredir',REPORT_DIR])
 
 
 
